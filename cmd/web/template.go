7e6b606432c8fe6571be2ed034bac7ba9f164a7c
Alfredo Mendoza <1930.kick@gmail.com>
2020-10-26 17:20:05 -0500

    add: Scaffolding

---
+++ cmd/web/template.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"html/template"
+	"io"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
+func humanDate(t time.Time) string {
+	if t.IsZero() {
+		return ""
+	}
+
+	return t.UTC().Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+	"humanDate": humanDate,
+}
+
+var t = &Template{
+	templates: template.Must(template.New("Expense").Funcs(functions).ParseGlob("ui/html/*.tmpl")),
+}
